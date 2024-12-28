5d1c53ac46661d46b32026aefdb3101fcdb80dcf
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-12-29 00:04:38 +0530

    fix: Enhance logout functionality by clearing JWT cookie with updated attributes for security

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -136,7 +136,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.cookie("jwt", "", { maxAge: 0 });
+        res.clearCookie("jwt", {
+            httpOnly: true,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: process.env.NODE_ENV === 'production',
+        });
+
         res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
         console.log("Error in logout controller", error.message);
