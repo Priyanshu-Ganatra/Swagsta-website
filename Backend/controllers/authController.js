0f4d53d87d5d673d965c62fdb841205ebf2b6ec3
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-12-28 23:37:52 +0530

    ..

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -136,9 +136,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.cookie('jwt', '', {
-            expires: new Date(0),
-        });
+        res.cookie("jwt", "", { maxAge: 0 });
         res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
         console.log("Error in logout controller", error.message);
--- Backend/lib/utils/generateToken.js
+++ Backend/lib/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (uid, res) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === 'production' && 'none',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         secure: process.env.NODE_ENV === 'production'
     })  
